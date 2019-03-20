ea0510ac304d19af70ce7971675adad2ab3a267f
Mat Munn <mat@matmunn.me>
2019-03-20 22:27:28 +1100

    Start tutorial

--- src/App.js
+++ src/App.js
@@ -1,28 +1,92 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import AceEditor from 'react-ace'
+import brace from 'brace'
+import styled from 'styled-components'
+import 'brace/mode/markdown'
+import 'brace/theme/cobalt'
+import Markdown from 'markdown-to-jsx'
+import './App.css'
+
+const { ipcRenderer } = window.require('electron')
 
 class App extends Component {
+  state = {
+    loadedFile: '',
+  }
+
+  constructor (props) {
+    super(props)
+
+    ipcRenderer.on('new-file', (event, data) => {
+      this.setState({
+        loadedFile: data
+      })
+    })
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <Header>
+          Journal
+        </Header>
+        <Split>
+          <CodeWindow>
+            <AceEditor
+              mode='markdown'
+              theme='cobalt'
+              onChange={(data) => {
+                this.setState({
+                  loadedFile: data,
+                })
+              }}
+              name='mardown_editor'
+              value={this.state.loadedFile}
+            />
+          </CodeWindow>
+          <RenderedWindow>
+            <Markdown>
+              {this.state.loadedFile}
+            </Markdown>
+          </RenderedWindow>
+        </Split>
       </div>
     );
   }
 }
 
 export default App;
+
+const Header = styled.header`
+  background-color: navy;
+  color: #ccc;
+  font-size: 0.8rem;
+  height: 23px;
+  text-align: center;
+  position: fixed;
+  top: 0;
+  box-shadow: 0 3px 3px rgba(0, 0, 0, 0.2);
+  left: 0;
+  width: 100%;
+  z-index: 10;
+  -webkit-app-region: drag;
+`
+
+const Split = styled.div`
+  display: flex;
+  height: 100vh;
+`
+
+const RenderedWindow = styled.div`
+  background-color: blue;
+  width: 35%;
+  padding: 20px;
+  color: #fff;
+  border-left: 1px solid green;
+`
+
+const CodeWindow = styled.div`
+  flex: 1;
+  padding-top: 2rem;
+  background-color: blue;
+`
